ef1b82a3d0e2098a2b6966a61a7e900ec121aabb
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-09 22:28:58 -0400

    First server commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ Hi: "There" });
+  res.send({ Hi: "React" });
 });
 
 //Do not change in future
