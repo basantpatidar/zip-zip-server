f4038a454d08fe0655496ed36babd65c60c107fb
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-10 23:09:34 -0400

    adding third authRoute

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,8 @@ module.exports = app => {
 
   //Access code from Google will be available at this route
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
