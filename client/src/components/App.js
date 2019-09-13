0e4b4df6edcf2118318b0153c9e37205f5851c81
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-12 22:05:23 -0400

    converted class component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,23 +7,26 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact={true} path="/" component={Landing}></Route>
-          <Route exact path="/surveys" component={Dashboard}></Route>
-          <Route path={"/surveys/new"} component={SurveyNew}></Route>
-        </div>
-      </BrowserRouter>
-      <a href="/auth/google">Sign In Google</a> <br></br>
-      <a href="/api/current_user">Current User</a>
-      <br></br>
-      <a href="/api/logout">logout</a>
-    </div>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact={true} path="/" component={Landing}></Route>
+            <Route exact path="/surveys" component={Dashboard}></Route>
+            <Route path={"/surveys/new"} component={SurveyNew}></Route>
+          </div>
+        </BrowserRouter>
+        <a href="/auth/google">Sign In Google</a> <br></br>
+        <a href="/api/current_user">Current User</a>
+        <br></br>
+        <a href="/api/logout">logout</a>
+      </div>
+    );
+  }
+}
 
 export default App;
