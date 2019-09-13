a543342bfe23ce2c0e944b3dd118c1dd47d68017
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-12 21:33:04 -0400

    react-router-dom wired  L80

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact={true} path="/" component={Landing}></Route>
+          <Route exact path="/surveys" component={Dashboard}></Route>
+          <Route path={"/surveys/new"} component={SurveyNew}></Route>
+        </div>
+      </BrowserRouter>
       <a href="/auth/google">Sign In Google</a> <br></br>
       <a href="/api/current_user">Current User</a>
       <br></br>
