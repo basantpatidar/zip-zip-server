2d2522cde0210c75eec5ad14068f87a459ff9c31
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-12 21:53:44 -0400

    Standard actionCreator with action not ReducThunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,4 +4,8 @@ import { FETCH_USER } from "./types";
 //fetchUser is action creator
 const fetchUser = async => {
  const response =  await axios.get("/api/current_user");
+ return{
+     types: FETCH_USER,
+     payload: response
+ };
 };
